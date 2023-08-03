fa9f23acc4b32e15d2baaf514ee83ce6f1abc936
williamNyongesa <marakawilliam@gmail.com>
2023-08-03 12:34:53 +0300

    done with part one

---
+++ script.js
@@ -0,0 +1,105 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const dataList = document.getElementById("datalistOptions");
+  const firstImage = document.querySelector(".col-md-4"); // Get the first element from the collection
+  const firstCardBody = document.querySelector(".card-body");
+  const filterContainer = document.getElementById("afterClickContainer");
+  const form = document.getElementById("locationForm");
+
+  // Fetch data from the server
+  function getData() {
+    fetch('http://localhost:3000/destinations')
+      .then(response => response.json())
+      .then(data => displayData(data))
+      .catch(error => alert(error));
+  }
+
+  // Display fetched data in the datalist and update the first destination content
+  function displayData(data) {
+    data.forEach(destination => {
+      const {
+        id,
+        place,
+        weather,
+        temperature,
+        description,
+        image
+      } = destination;
+
+      const dataListOption = document.createElement("option");
+      dataListOption.textContent = place;
+      dataList.appendChild(dataListOption);
+
+      if (id === 1) {
+        let myimage1 = document.createElement("div");
+        let firstCardBody1 = document.createElement("h6");
+
+        myimage1.innerHTML = `
+          <img src="${image}" alt="image of diani">
+        `;
+        firstCardBody1.innerHTML = `
+          <h4>${description}</h4>
+          <p>Weather: ${weather}</p>
+          <p>Temperature: ${temperature}</p>
+        `;
+
+        firstImage.innerHTML = ""; // Clear previous content
+        firstCardBody.innerHTML = ""; // Clear previous content
+        firstImage.appendChild(myimage1);
+        firstCardBody.appendChild(firstCardBody1);
+      }
+    });
+
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      let selectedPlace = document.getElementById("exampleDataList").value;
+      showPlace(selectedPlace);
+    });
+
+    // Function to display selected place information
+    function showPlace(selectedPlace) {
+      let selectedDestination = data.find(destination => destination.place === selectedPlace);
+      if (selectedDestination) {
+        let destinationImage = document.createElement("img");
+        destinationImage.src = selectedDestination.image;
+        firstImage.innerHTML = ""; // Clear previous content
+        firstImage.appendChild(destinationImage);
+
+        let destinationDescription = document.createElement("h5");
+        destinationDescription.textContent = selectedDestination.description;
+        firstCardBody.innerHTML = ""; // Clear previous content
+        firstCardBody.appendChild(destinationDescription);
+
+        let destinationWeather = document.createElement("p");
+        destinationWeather.textContent = `Weather: ${selectedDestination.weather}`;
+        firstCardBody.appendChild(destinationWeather);
+
+        let destinationTemperature = document.createElement("p");
+        destinationTemperature.textContent = `Temperature: ${selectedDestination.temperature}`;
+        firstCardBody.appendChild(destinationTemperature);
+      } else {
+        console.log("Place not found in the data!");
+      }
+    }
+
+    // Handle input selection
+    document.getElementById("exampleDataList").addEventListener("input", function (event) {
+      let selectedInputValue = event.target.value;
+      let selectedDestination = data.find(destination => destination.place === selectedInputValue);
+      if (selectedDestination) {
+        let destinationElement = document.createElement("div");
+        destinationElement.innerHTML = `
+          <h4>${selectedDestination.place}</h4>
+          <img src="${selectedDestination.image}" alt="Image of ${selectedDestination.place}">
+          <p>${selectedDestination.description}</p>
+          <p><strong>Weather: ${selectedDestination.weather}</strong></p>
+          <p>Temperature: ${selectedDestination.temperature}</p>
+        `;
+        filterContainer.innerHTML = ""; // Clear previous content
+        filterContainer.appendChild(destinationElement);
+      }
+    });
+  }
+
+  // Fetch data from the server and display it in the datalist
+  getData();
+});
